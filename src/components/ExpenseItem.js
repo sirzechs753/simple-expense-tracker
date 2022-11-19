95e341980d46b1e08f9b04cf8489eb95609875be
sirzechs753 <sirzechs753@gmail.com>
2022-11-19 12:25:59 +0700

    react-thecompleteguide-34

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return (
+        <div className="">
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
