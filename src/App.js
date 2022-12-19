998a5e8a8b9ca069792c45a92717d2da0de60d63
Sirzechs753 <sirzechs753@gmail.com>
2022-12-19 09:26:26 +0700

    react-thecompleteguide-58

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import React, { useSatae } from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
+
+  // const [expenses, setExpenses] = useSatae([
+  //   {
+  //     id: "e1",
+  //     title: "Toilet Paper",
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
+  //   },
+  //   { 
+  //     id: "e2", 
+  //     title: "New TV", 
+  //     amount: 799.49, 
+  //     date: new Date(2021, 2, 12) 
+  //   },
+  //   {
+  //     id: "e3",
+  //     title: "Car Insurance",
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     id: "e4",
+  //     title: "New Desk (Wooden)",
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   }
+  // ])
 
   const expenses = [
     {
@@ -29,9 +58,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
